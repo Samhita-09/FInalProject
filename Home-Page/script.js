1ca9fb523a3ffca0808561b923246a3296884c3e
Samhita <samkal28@bergen.org>
2025-06-02 14:54:55 -0400

    tried to do copy buttons
    Co-authored-by: JulianaZagaja <JulianaZagaja@users.noreply.github.com>

--- Home-Page/script.js
+++ Home-Page/script.js
@@ -1,6 +1,15 @@
-const searchbar = document.querySelector("input")
+// const searchbar = document.querySelector(".form-input")
 const buttons = document.querySelectorAll("button")
+const selectDemo = document.querySelector("#item-input")
 
-function copyEmail() {
-    
-}
+selectDemo.addEventListener("select", function(){
+    const selectedText = this.value.substring(this.selectionStart, this.selectionEnd)
+    buttons.textContent = `Selected Text: ${selectedText}`
+})
+selectDemo.addEventListener("copy", function(){
+    buttons.textContent = "Text copied to the clipboard"
+    setTimeout(() => {
+        const selectedText = this.value.substring(this.selectionStart, this.selectionEnd)
+        buttons.textContent = `Selected Text: ${selectedText}`
+    }, 2000)
+})
